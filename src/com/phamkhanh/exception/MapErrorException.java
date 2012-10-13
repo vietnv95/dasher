7a8ab4a57d69b1abb01f6231a30013ccb9353cd9
phamvankhanhbka <phamvankhanhbka@db798f4c-c626-e033-bb63-5352bd5ffc4f>
2012-10-13 17:15:32 +0000

    Thêm Custom Exception để quản lí lỗi

---
+++ src/com/phamkhanh/exception/MapErrorException.java
@@ -0,0 +1,7 @@
+package com.phamkhanh.exception;
+
+public class MapErrorException extends Exception{
+	public MapErrorException(String message){
+		super(message);
+	}
+}
---
+++ src/com/phamkhanh/exception/SaveNotSuccessException.java
@@ -0,0 +1,7 @@
+package com.phamkhanh.exception;
+
+public class SaveNotSuccessException extends Exception {
+	public SaveNotSuccessException(String message){
+		super(message);
+	}
+}
